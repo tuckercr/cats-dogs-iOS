956d4a61869d26b925cba2de23409fde13904d5a
Colin Tucker <colinrtucker@gmail.com>
2026-06-04 12:56:37 -0400

    Initial Commit

---
+++ Cats&Dogs/Cats_DogsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cats_DogsApp.swift
+//  Cats&Dogs
+//
+//  Created by Colin Tucker on 6/4/26.
+//
+
+import SwiftUI
+
+@main
+struct Cats_DogsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cats&Dogs/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Cats&Dogs
+//
+//  Created by Colin Tucker on 6/4/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
